55f3948f68ba1bf3ea259b20756c73e048d19c0a
LoliDens <104903662+LoliDens@users.noreply.github.com>
2022-10-14 18:53:38 +0500

    Добавьте файлы проекта.

---
+++ homeWork_swap/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork_swap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Фазлетдинов";
+            string surname = "Данил";
+            string swapText;
+
+            Console.WriteLine($"Имя: {name}.\nФамилия: {surname}");
+
+            swapText = name;
+            name = surname;
+            surname = swapText;
+            Console.WriteLine($"Имя: {name}.\nФамилия: {surname}");
+            Console.ReadKey();
+        }
+    }
+}
